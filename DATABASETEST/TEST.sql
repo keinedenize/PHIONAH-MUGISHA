01c2647119896d5986d60ad179314b0eabb15760
keinedenize <keineprecious@gmail.com>
2025-04-23 12:29:50 +0300

    Add files via upload

---
+++ DATABASETEST/TEST.sql
@@ -0,0 +1,27 @@
+
+CREATE TABLE Finance (
+    StudentID INT,
+    Tuition_Fee DECIMAL,
+    Administration_Fee DECIMAL,
+    Exams_Fee DECIMAL,
+    Medical_Fee DECIMAL
+);
+
+SELECT StudentName, StudentID, Tuition_Fee
+FROM tblStudent s
+WHERE Year = 'Y1S1';
+
+
+SELECT StudentName, StudentID, Year, Tuition_Fee
+FROM tblStudent 
+WHERE f.Tuition_Fee >= 30000;
+
+INSERT INTO tblStudent (StudentName, StudentID, Birthdate, Year, MobileNo)
+VALUES ('Dan', '2001-04-20', 'Y1S2', '0723456123');
+
+
+UPDATE tblStudent
+SET StudentName = 'Nelson'
+WHERE StudentID = 26;
+
+
